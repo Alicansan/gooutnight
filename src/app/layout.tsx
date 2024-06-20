89437824f1d027c597cadee9b4dfe85c9b4b0933
Alican Şan <alicansan2606@gmail.com>
2024-06-21 01:06:45 +0300

    no scrollbar from global

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang='en ' className='no-scrollbar'>
+    <html lang='en '>
       <body
         className={
           inter.className +
