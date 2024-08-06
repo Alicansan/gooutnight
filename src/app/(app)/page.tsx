ee79295134a2c414795342839ff41642ccdf2524
ariburaco <34109588+ariburaco@users.noreply.github.com>
2024-08-06 15:31:13 +0300

    feat: use payload client sdk

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -4,15 +4,24 @@ import { FeaturedVenuesAlternativeCardsSection } from "@/components/sections/Fea
 import { FeaturedVenuesSection } from "@/components/sections/FeaturedVenuesSection";
 import { HeroSection } from "@/components/sections/HeroSection";
 import SearchSection from "@/components/sections/SearchSection";
+import { getPayloadHMR } from "@payloadcms/next/utilities";
+import configPromise from "@payload-config";
 
-export default function Home() {
+export default async function Home() {
+  const payload = await getPayloadHMR({ config: configPromise });
+
+  const places = await payload.find({
+    collection: "places",
+  });
+
+  // TODO: pass the places to the FeaturedVenuesAlternativeCardsSection component
   return (
     <main className="flex flex-col items-center justify-between overflow-hidden">
       <HeroSection />
       <SearchSection />
       <FeaturedVenuesSection />
       <CompactMap />
-      <FeaturedVenuesAlternativeCardsSection />
+      {/* <FeaturedVenuesAlternativeCardsSection /> */}
       <ComingSoon />
     </main>
   );
--- src/components/sections/FeaturedVenuesAlternativeCardsSection.tsx
+++ src/components/sections/FeaturedVenuesAlternativeCardsSection.tsx
@@ -2,9 +2,9 @@
 
 import Image from "next/image";
 import Link from "next/link";
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 
-import {BackgroundGradient} from "@/components/ui/background-gradient";
+import { BackgroundGradient } from "@/components/ui/background-gradient";
 
 interface Venue {
   id: number;
@@ -32,32 +32,33 @@ export function FeaturedVenuesAlternativeCardsSection() {
     fetchData();
   }, []);
 
-  if(venues.length === 0) return null;
+  if (venues?.length === 0) return null;
 
   return (
     <div className="container grid h-full w-full grid-cols-1 gap-10 bg-background py-10 sm:grid-cols-2 lg:grid-cols-3">
-      {venues.map((venue) => (
-        <Link href={venue.link} key={venue.id}>
-          <BackgroundGradient className="rounded-[22px] bg-white pb-4 dark:bg-zinc-900">
-            <Image
-              src={venue.src}
-              alt={venue.alt}
-              height="500"
-              width="500"
-              className="h-[265px] max-h-[auto] max-w-[auto] rounded-t-[23px] border-b-2 border-yellow-800 object-contain"
-            />
-            <div className="p-3">
-              <p className="mb-2 mt-4 text-base text-black dark:text-neutral-200 sm:text-xl">
-                {venue.name}
-              </p>
+      {venues &&
+        venues?.map((venue) => (
+          <Link href={venue.link} key={venue.id}>
+            <BackgroundGradient className="rounded-[22px] bg-white pb-4 dark:bg-zinc-900">
+              <Image
+                src={venue.src}
+                alt={venue.alt}
+                height="500"
+                width="500"
+                className="h-[265px] max-h-[auto] max-w-[auto] rounded-t-[23px] border-b-2 border-yellow-800 object-contain"
+              />
+              <div className="p-3">
+                <p className="mb-2 mt-4 text-base text-black dark:text-neutral-200 sm:text-xl">
+                  {venue.name}
+                </p>
 
-              <p className="pb-3 text-sm text-neutral-600 dark:text-neutral-400">
-                {venue.address}
-              </p>
-            </div>
-          </BackgroundGradient>
-        </Link>
-      ))}
+                <p className="pb-3 text-sm text-neutral-600 dark:text-neutral-400">
+                  {venue.address}
+                </p>
+              </div>
+            </BackgroundGradient>
+          </Link>
+        ))}
     </div>
   );
 }
